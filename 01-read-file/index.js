1cba9fa6c2ef6f74e3b1e50ade3a272aca2e336a
scheslen <antgalin@mail.ru>
2024-01-20 22:05:09 +0300

    feat: implement 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+
+const txtStream = fs.createReadStream(path.join(__dirname,'text.txt'),'utf-8');
+
+txtStream.on('data', (data) => stdout.write(data));
+
