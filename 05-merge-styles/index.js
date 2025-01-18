b9ba51d032a25155b697798714f58409688f29fc
scheslen <antgalin@mail.ru>
2025-01-18 17:35:26 +0300

    refactor: correct 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,32 +1,32 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
 const inFolder = path.join(__dirname, 'styles');
 const outFile = path.join(__dirname, 'project-dist', 'bundle.css');
 
-let fileWriteStream = fs.createWriteStream(outFile);
-fileWriteStream.end();
+let fileWriteStream = fs.createWriteStream(outFile)
+fileWriteStream.end()
 
-let txtReadStream;
+let txtReadStream
 
 fs.readdir(inFolder, 'utf8', (err, aFiles) => {
-  if (err) throw err;
+  if (err) throw err
   for (let i in aFiles) {
     fs.stat(path.join(inFolder, aFiles[i]), function (err, stats) {
-      if (err) throw err;
+      if (err) throw err
 
       let sFN = aFiles[i];
       if (stats.isFile() && path.extname(sFN) === '.css') {
         txtReadStream = fs.createReadStream(
           path.join(__dirname, 'styles', sFN),
           'utf-8',
-        ); //__dirname+'\\styles\\'+sFN
+        ) //__dirname+'\\styles\\'+sFN
         txtReadStream.on('data', function (chunk) {
           fs.appendFile(outFile, chunk, (err) => {
-            if (err) throw err;
-          });
-        });
+            if (err) throw err
+          })
+        })
       }
-    });
+    })
   }
-});
+})
