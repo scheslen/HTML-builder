5f843452eba9d05c5dd83aafbffc5a8dd473be5f
scheslen <antgalin@mail.ru>
2025-01-18 11:51:56 +0300

    refactor: correct 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-const { stdout } = process;
+const fs = require('fs')
+const path = require('path')
+const { stdout } = require('process')
 
-const txtStream = fs.createReadStream(path.join(__dirname, 'text.txt'),'utf-8');
-
-txtStream.on('data', (data) => stdout.write(data));
+const txtStream = fs.createReadStream(path.join(__dirname, 'text.txt'),'utf-8')
+txtStream.on('data', (data) => stdout.write(data))
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,29 +1,29 @@
-const fs = require('fs');
-const path = require('path');
-const process = require('process');
-const readline = require('readline');
+const fs = require('fs')
+const path = require('path')
+const process = require('process')
+const readline = require('readline')
 
-let fileWriteStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+let fileWriteStream = fs.createWriteStream(path.join(__dirname, 'text.txt'))
 let readInterface = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-console.log('Hello! Input text, please. Ctrl+C or "exit" - end of input.');
+console.log('Hello! Input text, please. Ctrl+C or "exit" - end of input.')
 
-readInterface.setPrompt('> ');
+readInterface.setPrompt('> ')
 readInterface.prompt();
 
 readInterface.on('line', (input) => {
   if (input.toLowerCase().trim() === 'exit') {
-    readInterface.close();
+    readInterface.close()
   } else {
-    fileWriteStream.write(input + '\n');
+    fileWriteStream.write(input + '\n')
     readInterface.prompt();
   }
 });
 
 readInterface.on('close', () => {
-  console.log('--- Input is over. Thank you.');
-  fileWriteStream.end();
+  console.log('--- Input is over. Thank you.')
+  fileWriteStream.end()
 });
