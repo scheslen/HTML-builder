7734d452a5428faba98965c7ec2d7ebe597d01fe
scheslen <antgalin@mail.ru>
2025-01-18 17:40:18 +0300

    refactor: correct 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,20 +1,20 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
 const distFolder = path.join(__dirname, 'project-dist'); //__dirname\\project-dist
 
-const oHtmlComponents = {};
-let sHtmlIndex = '';
-let qComponents = 0;
+const oHtmlComponents = {}
+let sHtmlIndex = ''
+let qComponents = 0
 
 fs.mkdir(distFolder, { recursive: true }, (err) => {
-  if (err) throw err;
-  createHtml();
+  if (err) throw err
+  createHtml()
 
-  createStyle( path.join(__dirname, 'styles'), path.join(distFolder, 'style.css'),);
+  createStyle( path.join(__dirname, 'styles'), path.join(distFolder, 'style.css'),)
 
-  copyDir(path.join(__dirname, 'assets'), path.join(distFolder, 'assets'));
-});
+  copyDir(path.join(__dirname, 'assets'), path.join(distFolder, 'assets'))
+})
 
 // ..................................................................
 
@@ -22,29 +22,29 @@ function createHtml() {
   let txtReadTempStream = fs.createReadStream(
     path.join(__dirname, 'template.html'),
     'utf-8',
-  );
+  )
 
-  let htmlTemplate = '';
+  let htmlTemplate = ''
   let pTag = -1;
   let pTagEnd = 0;
 
   txtReadTempStream.on('data', function (tempChunk) {
-    htmlTemplate = tempChunk;
-    sHtmlIndex += htmlTemplate;
+    htmlTemplate = tempChunk
+    sHtmlIndex += htmlTemplate
 
     while (htmlTemplate.length > 0) {
-      pTag = htmlTemplate.indexOf('{{');
+      pTag = htmlTemplate.indexOf('{{')
 
       if (pTag < 0) {
-        htmlTemplate = '';
+        htmlTemplate = ''
       } else {
         pTagEnd = htmlTemplate.indexOf('}}');
-        oHtmlComponents[htmlTemplate.slice(pTag + 2, pTagEnd)] = '';
-        qComponents += 1;
-        htmlTemplate = htmlTemplate.slice(pTagEnd + 2);
+        oHtmlComponents[htmlTemplate.slice(pTag + 2, pTagEnd)] = ''
+        qComponents += 1
+        htmlTemplate = htmlTemplate.slice(pTagEnd + 2)
       }
     }
-  });
+  })
 
   txtReadTempStream.on('end', () => {
     for (let key in oHtmlComponents) {
@@ -52,39 +52,39 @@ function createHtml() {
         path.join(__dirname, 'components', `${key}.html`),
         'utf8',
         function (err, txtComponent) {
-          if (err) throw err;
-          oHtmlComponents[key] = txtComponent;
-          qComponents -= 1;
-          if (qComponents === 0) writeHtml();
+          if (err) throw err
+          oHtmlComponents[key] = txtComponent
+          qComponents -= 1
+          if (qComponents === 0) writeHtml()
         })
     }
-  });
+  })
 
   function writeHtml() {
-    let fileWriteStream = fs.createWriteStream(path.join(distFolder, 'index.html'),{encoding: 'utf8'});
+    let fileWriteStream = fs.createWriteStream(path.join(distFolder, 'index.html'),{encoding: 'utf8'})
 
     for (let key in oHtmlComponents) {
-      sHtmlIndex = sHtmlIndex.replace(`{{${key}}}`, oHtmlComponents[key]);
+      sHtmlIndex = sHtmlIndex.replace(`{{${key}}}`, oHtmlComponents[key])
     }
-    fileWriteStream.write(sHtmlIndex);
-    fileWriteStream.end();
-  };
+    fileWriteStream.write(sHtmlIndex)
+    fileWriteStream.end()
+  }
 }
 
 // ..................................................................
 
 function createStyle(styleFolder, styleFile) {
-  let fileWriteStream = fs.createWriteStream(styleFile);
-  fileWriteStream.end();
-  let txtReadStream;
+  let fileWriteStream = fs.createWriteStream(styleFile)
+  fileWriteStream.end()
+  let txtReadStream
 
   fs.readdir(styleFolder, 'utf8', (err, aFiles) => {
-    if (err) throw err;
+    if (err) throw err
     for (let i in aFiles) {
       fs.stat(path.join(styleFolder, aFiles[i]), function (err, stats) {
-        if (err) throw err;
+        if (err) throw err
 
-        let sFN = aFiles[i];
+        let sFN = aFiles[i]
         if (stats.isFile() && path.extname(sFN) === '.css') {
           txtReadStream = fs.createReadStream(
             path.join(styleFolder, sFN),
@@ -92,35 +92,34 @@ function createStyle(styleFolder, styleFile) {
           );
           txtReadStream.on('data', function (chunk) {
             fs.appendFile(styleFile, chunk, (err) => {
-              if (err) throw err;
-            });
-          });
+              if (err) throw err
+            })
+          })
         }
-      });
+      })
     }
-  });
+  })
 }
 
 // ..................................................................
 
 function copyDir(inFolder, outFolder) {
   fs.mkdir(outFolder, { recursive: true }, (err) => {
-    if (err) throw err;
+    if (err) throw err
 
     fs.readdir(inFolder, 'utf8', (err, aFiles) => {
-      if (err) throw err;
+      if (err) throw err
 
       for (let i in aFiles) {
         fs.stat(path.join(inFolder, aFiles[i]), function (err, stats) {
-          if (err) throw err;
-          // if (err) console.error(err);
+          if (err) throw err
 
           if (stats.isFile()) {
             fs.copyFile(
               path.join(inFolder, aFiles[i]),
               path.join(outFolder, aFiles[i]),
               (err) => {
-                if (err) throw err;
+                if (err) throw err
               },
             );
           }
@@ -128,10 +127,10 @@ function copyDir(inFolder, outFolder) {
             copyDir(
               path.join(inFolder, aFiles[i]),
               path.join(outFolder, aFiles[i])
-            );
+            )
           }
-        });
+        })
       }
-    });
-  });
+    })
+  })
 }
